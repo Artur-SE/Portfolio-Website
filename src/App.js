396bbbf4dcaca2bf559934eec9820e6e7482a704
Artur-SE <arturklein512@googlemail.com>
2021-10-08 20:13:38 +0200

    Structure added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Questions from "./components/Questions";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Services />
+      <AboutMe />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Questions />
+    </div>
+  );
 }
 
 export default App;
