6f3d476b6599870a8da66764a8129e4b22254441
Artur-SE <arturklein512@googlemail.com>
2021-10-19 18:31:46 +0200

    hero added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,54 @@
 import React from "react";
 
+// Styling
+import styled from "styled-components";
+
+// Assets
+import heroImage from "../assets/hero.svg";
+
 const Home = () => {
+  // Return -------------------------------------------------------------------
+
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
+    <StyledHero>
+      <div className="container">
+        <p className="headline">Wir bauen & unterstützen Ihr Traumprojekt</p>
+        <p>
+          <span>W</span>
+          ebsites,
+          <span> P</span>
+          rogramme,
+          <span> D</span>
+          esigns und mehr...
+        </p>
+        <img src={heroImage} alt="responsive design" className="hero-image" />
+        <button className="btn-primary">Kostenlose Beratung</button>
+      </div>
+    </StyledHero>
   );
+  // --------------------------------------------------------------------------
 };
+
+const StyledHero = styled.section`
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 0 1rem;
+
+  .container {
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .headline {
+    font-size: 2.5rem;
+    color: var(--textColor1);
+  }
+
+  .hero-image {
+    width: 80%;
+    padding: 3rem 0;
+  }
+`;
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,53 +4,122 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 // Assets
-import Logo from "../assets/logo.svg";
+import logo from "../assets/logo.svg";
 import navIcon from "../assets/navIcon.svg";
 
 const Navbar = () => {
   // State ----------------------------------------------------------------------
-  const [showNavigation, setShowNavigation] = useState(false);
+
+  const [showDropDown, setShowNavigation] = useState(false);
   // ----------------------------------------------------------------------------
 
   const toggleNavigation = () => {
-    setShowNavigation(!showNavigation);
+    setShowNavigation(!showDropDown);
   };
+
+  // Return ---------------------------------------------------------------------
 
   return (
     <StyledNavbar>
-      <img src={Logo} alt="Smart Development" className="logo" />
-      <img
-        onClick={toggleNavigation}
-        src={navIcon}
-        alt="navIcon"
-        className="navIcon"
-      />
-      {showNavigation && (
-        <ul>
-          <li>Home</li>
-          <li>Features</li>
-          <li>Services</li>
-          <li>About</li>
-          <li>Kontakt</li>
-        </ul>
-      )}
+      <nav className="container">
+        <div className="navbar">
+          <img src={logo} alt="Smart Development" className="logo" />
+
+          <img
+            onClick={toggleNavigation}
+            src={navIcon}
+            alt="navIcon"
+            className="navIcon"
+          />
+          <ul className="nav-links-wideScreen">
+            <li>Home</li>
+            <li>Features</li>
+            <li>Services</li>
+            <li>About</li>
+            <li>
+              <span>Contact</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="nav-links-mobile">
+          {showDropDown && (
+            <ul>
+              <li>Home</li>
+              <li>Features</li>
+              <li>Services</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          )}
+        </div>
+      </nav>
     </StyledNavbar>
   );
+  // ----------------------------------------------------------------------------
 };
 
 // Styled Components
+
 const StyledNavbar = styled.section`
-  display: flex;
-  justify-content: space-between;
-  background-color: var(--backgroundColor2);
-  padding: 0.5rem 1rem;
+  .container {
+    background-color: var(--backgroundColor2);
+  }
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 1rem;
+    height: 3rem;
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+  }
+  .nav-links-wideScreen {
+    display: none;
+  }
 
   .logo {
     width: 15rem;
   }
+
   .navIcon {
     width: 2rem;
   }
+
+  .nav-links-mobile {
+    background-color: var(--backgroundColor2);
+    ul {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      text-align: center;
+    }
+    li {
+      color: var(--textColor1);
+      padding: 0.7rem 0;
+    }
+  }
+
+  /* Widescreen View ---------------------------------------------------------- */
+  @media (min-width: 1023px) {
+    .navIcon {
+      display: none;
+    }
+    .nav-links-wideScreen {
+      display: flex;
+      ul {
+        margin: 0;
+        padding: 0;
+        text-align: center;
+      }
+      li {
+        color: var(--textColor1);
+        list-style: none;
+        padding: 0 1rem;
+      }
+    }
+  }
+  /* -------------------------------------------------------------------------- */
 `;
+// ----------------------------------------------------------------------------
 
 export default Navbar;
