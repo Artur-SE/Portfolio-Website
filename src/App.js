7f41fbf4916dcf93c97ea70310843a20f72bd430
Artur-SE <arturklein512@googlemail.com>
2021-10-19 13:18:36 +0200

    setup ready

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import Hero from "./components/Hero";
 import Services from "./components/Services";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
-import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
-import Questions from "./components/Questions";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <Services />
       <AboutMe />
       <Projects />
-      <Testimonials />
       <Contact />
-      <Questions />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,34 @@
 import React from "react";
 
+// Styling
+import styled from "styled-components";
+
+// Assets
+import Logo from "../assets/logo.svg";
+import navIcon from "../assets/navIcon.svg";
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <StyledNavbar>
+      <img src={Logo} alt="Smart Development" className="logo" />
+      <img src={navIcon} alt="navIcon" className="navIcon" />
+    </StyledNavbar>
   );
 };
+
+// Styled Components
+const StyledNavbar = styled.section`
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--backgroundColor2);
+  padding: 0.5rem 1rem;
+
+  .logo {
+    width: 15rem;
+  }
+  .navIcon {
+    width: 2.5rem;
+  }
+`;
 
 export default Navbar;
